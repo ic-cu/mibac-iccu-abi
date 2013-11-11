ac22646d3739271649c20854d2bedf3d7b070328
iccu.andrea.giuliano <iccu.andrea.giuliano@gmail.com>
2013-11-11 14:17:52 +0100

    - Ora il quarto parametro non è più un file, ma una directory. Per ora non è più possibile scegliere un singolo file, ma basta metterlo in una directory a parte.

--- xml/VarieQuery.java
+++ xml/VarieQuery.java
@@ -88,7 +88,7 @@ public class VarieQuery
 
 			String query = "select " + campo + " from " + tabella;
 			query += " where " + campo + " = \'" + valore2 + "\'";
-			//out(query);
+			// out(query);
 			ResultSet rs = stmt.executeQuery(query);
 			rs.last();
 			if(rs.getRow() != 1)
@@ -131,29 +131,56 @@ public class VarieQuery
 		String user = args[2];
 		String pass = args[3];
 		String xml = args[4];
+		String riga;
+		String parti[];
+		String xpath;
+		String tabella = null;
+		String campo = null;
 		VarieQuery vq = new VarieQuery(driver, url, user, pass);
-		Document doc = vq.load(new File(xml));
+		File[] dir = new File(xml).listFiles();
+		Set ts = null;
+		Document doc = null, doc2;
+		boolean trovato = false;
+		for(int ii = 0; ii < dir.length; ii++)
+		{
+			if(dir[ii].isFile())
+			{
+				doc2 = vq.load(dir[ii]);
+				Iterator ibib = doc2.getRootElement().getChildren("biblioteca")
+						.iterator();
+				if(!trovato)
+				{
+					doc = (Document) doc2.clone();
+					trovato = true;
+				}
+				else
+					while(ibib.hasNext())
+					{
+						Element bib = (Element) ibib.next();
+						doc.getRootElement().addContent(bib.cloneContent());
+					}
+			}
+		}
 		try
 		{
 			BufferedReader conf = new BufferedReader(
 					new FileReader("VarieQuery.conf"));
 			while(conf.ready())
 			{
-				String riga = conf.readLine();
-				String parti[] = riga.split(" ");
-				String xpath = parti[0];
-				String tabella = parti[1];
-				String campo = parti[2];
+				riga = conf.readLine();
+				parti = riga.split(" ");
+				xpath = parti[0];
+				tabella = parti[1];
+				campo = parti[2];
 				vq.out("Percorso: " + xpath);
 				try
 				{
 					XPath xp = XPath.newInstance(xpath);
 					List list = xp.selectNodes(doc);
-					Set ts = new TreeSet();
-					//vq.out(" " + list.size());
+					// vq.out(" " + list.size());
 
 					// itera sulla lista dei nodi estratti dal xpath
-
+					ts = new TreeSet();
 					ListIterator li = list.listIterator();
 					while(li.hasNext())
 					{
@@ -196,14 +223,14 @@ public class VarieQuery
 
 					}
 
-					// itera sul set, quindi stessi dati, ma in ordine e senza ripetizioni
+					// itera sul set, quindi stessi dati, ma in ordine e senza
+					// ripetizioni
 					// (e solo il contenuto)
-
 					Iterator i = ts.iterator();
 					while(i.hasNext())
 					{
 						String valore = (String) i.next();
-						// vq.out(valore);
+						//vq.out(tabella + ", " + campo + ", " + valore);
 						vq.verificaValore(tabella, campo, valore);
 					}
 				}
